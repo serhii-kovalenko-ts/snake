9ea9c44829e6ef8ae74bfac158613db4e0b5ff6f
Serhii Kovalenko <kovalenkos@mydigicode.com>
2026-09-19 21:05:29 +0300

    fix build

--- src/main.ts
+++ src/main.ts
@@ -5,29 +5,34 @@ import { LoadManager } from "./managers/LoadManager";
 import { SoundManager } from "./managers/SoundManager";
 import { CanvasRenderer } from "./render/CanvasRenderer";
 
-const canvas = document.querySelector<HTMLCanvasElement>("#game");
-const adContainer = document.querySelector<HTMLElement>("#ad-container");
+async function init(): Promise<void> {
+  const canvas = document.querySelector<HTMLCanvasElement>("#game");
+  const adContainer = document.querySelector<HTMLElement>("#ad-container");
 
-if (!canvas) {
-  throw new Error("Game canvas was not found.");
+  if (!canvas) {
+    throw new Error("Game canvas was not found.");
+  }
+
+  if (!adContainer) {
+    throw new Error("Ad container was not found.");
+  }
+
+  const loadManager = new LoadManager();
+  await loadManager.loadAssets();
+
+  const renderer = new CanvasRenderer(canvas, loadManager.getImages());
+  const sound = new SoundManager(loadManager.getSounds());
+  const input = new InputManager();
+  const ads = new AdsManager(adContainer);
+
+  const game = new Game({
+    renderer,
+    input,
+    sound,
+    ads,
+  });
+
+  game.start();
 }
-if (!adContainer) {
-  throw new Error("Ad container was not found.");
-}
 
-const loadManager = new LoadManager();
-await loadManager.loadAssets();
-
-const renderer = new CanvasRenderer(canvas, loadManager.getImages());
-const sound = new SoundManager(loadManager.getSounds());
-const input = new InputManager();
-const ads = new AdsManager(adContainer);
-
-const game = new Game({
-  renderer,
-  ads,
-  input,
-  sound,
-});
-
-game.start();
+void init();
